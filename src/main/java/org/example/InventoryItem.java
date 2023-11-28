be6659958289f752ca31d6af9b40080b70d9258d
Jesús Plou <jesusplou@gmail.com>
2023-11-28 19:45:51 +0100

    Refactor age method to new class

---
+++ src/main/java/org/example/InventoryItem.java
@@ -0,0 +1,86 @@
+package org.example;
+
+import java.util.Objects;
+
+public final class InventoryItem {
+    private final Item item;
+
+    public InventoryItem(Item item) {
+        this.item = item;
+    }
+
+    void age() {
+        if (!item().name.equals("Aged Brie")
+                && !item().name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item().quality > 0) {
+                if (!item().name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item().quality = item().quality - 1;
+                }
+            }
+        } else {
+            if (item().quality < 50) {
+                item().quality = item().quality + 1;
+
+                if (item().name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item().sellIn < 11) {
+                        if (item().quality < 50) {
+                            item().quality = item().quality + 1;
+                        }
+                    }
+
+                    if (item().sellIn < 6) {
+                        if (item().quality < 50) {
+                            item().quality = item().quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!item().name.equals("Sulfuras, Hand of Ragnaros")) {
+            item().sellIn = item().sellIn - 1;
+        }
+
+        if (item().sellIn < 0) {
+            if (!item().name.equals("Aged Brie")) {
+                if (!item().name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item().quality > 0) {
+                        if (!item().name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item().quality = item().quality - 1;
+                        }
+                    }
+                } else {
+                    item().quality = item().quality - item().quality;
+                }
+            } else {
+                if (item().quality < 50) {
+                    item().quality = item().quality + 1;
+                }
+            }
+        }
+    }
+
+    public Item item() {
+        return item;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (InventoryItem) obj;
+        return Objects.equals(this.item, that.item);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(item);
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryItem[" +
+                "item=" + item + ']';
+    }
+
+}
