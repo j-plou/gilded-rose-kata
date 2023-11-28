b6d5e2969bf9f240a7901f3ba1624e64bb1b549d
Jesús Plou <jesusplou@gmail.com>
2023-11-28 18:53:42 +0100

    Add first test!

--- src/test/java/org/example/GildedRoseTest.java
+++ src/test/java/org/example/GildedRoseTest.java
@@ -7,6 +7,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class GildedRoseTest {
     @Test
     void test() {
-        assertThat(true).isTrue();
+        final Item item = new Item("foo", 5, 5);
+        final GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isEqualTo(4);
+        assertThat(item.sellIn).isEqualTo(4);
     }
 }
